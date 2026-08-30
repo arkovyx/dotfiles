1c7efc4a928a8f04957bfb908c6de5ef2fb18e7a
arkovyx <me.arkovyx@gmail.com>
2026-08-30 11:39:39 +0530

    add ss dirs

--- .local/bin/hypr/hypr_screenshot.sh
+++ .local/bin/hypr/hypr_screenshot.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
-ocr_cmd="wl-copy"
-
-case "$(printf "a selected area (copy)\ncurrent window (copy)\nfull screen (copy)\na selected area\na selected area (tmp dir)\ncurrent window\nfull screen\na selected area (OCR)" | fuzzel -d -l 7 -i -p "Screenshot which area?")" in
+case "$(printf "a selected area (copy)\ncurrent window (copy)\nfull screen (copy)\na selected area (ss)\na selected area (kl)\ncurrent window (ss)\ncurrent window (mpv)\na selected area (mpv)" | fuzzel -d -l 8 -i -p "Screenshot which area?")" in
     "a selected area (copy)") hyprshot -m region --clipboard-only ;;
     "current window (copy)") hyprshot -m window --clipboard-only ;;
     "full screen (copy)") hyprshot -m output --clipboard-only ;;
 
-    "a selected area") hyprshot -m region -o ~/pix/ss -f "pic-selected-$(uuidgen | awk -F- '{printf $2}')-$(date '+%y-%m-%d').png" ;;
-    "a selected area (mpv)") hyprshot -m region -o ~/pix/ss/mpv -f "pic-selected-$(uuidgen | awk -F- '{printf $2}')-$(date '+%y-%m-%d').png" ;;
-    "a selected area (kl)") hyprshot -m region -o ~/pix/ss/kl -f "pic-selected-$(uuidgen | awk -F- '{printf $2}')-$(date '+%y-%m-%d').png" ;;
-    "current window") hyprshot -m window -o ~/pix/ss -f "pic-window-$(uuidgen | awk -F- '{printf $2}')-$(date '+%y-%m-%d').png" ;;
-    "full screen") hyprshot -m output -o ~/pix/ss -f "pic-full-$(uuidgen | awk -F- '{printf $2}')-$(date '+%y-%m-%d').png" ;;
-
-    "a selected area (OCR)") tmpfile=$(mktemp /tmp/ocr-XXXXXX.png) && \
-        slurp | grim -g - - | tesseract stdin stdout -l eng | wl-copy
+    "a selected area (ss)") mkdir -p ~/pix/ss && hyprshot -m region -o ~/pix/ss -f "pic-selected-$(uuidgen | awk -F- '{printf $2}')-$(date '+%y-%m-%d').png" ;;
+    "a selected area (kl)") mkdir -p ~/pix/ss/kl && hyprshot -m region -o ~/pix/ss/kl -f "pic-selected-$(uuidgen | awk -F- '{printf $2}')-$(date '+%y-%m-%d').png" ;;
+    "current window (ss)") mkdir -p ~/pix/ss && hyprshot -m window -o ~/pix/ss -f "pic-window-$(uuidgen | awk -F- '{printf $2}')-$(date '+%y-%m-%d').png" ;;
+    "current window (mpv)") mkdir -p ~/pix/ss/mpv && hyprshot -m window -o ~/pix/ss/mpv -f "pic-window-$(uuidgen | awk -F- '{printf $2}')-$(date '+%y-%m-%d').png" ;;
+    "a selected area (mpv)") mkdir -p ~/pix/ss/mpv && hyprshot -m region -o ~/pix/ss/mpv -f "pic-selected-$(uuidgen | awk -F- '{printf $2}')-$(date '+%y-%m-%d').png" ;;
 esac
-
